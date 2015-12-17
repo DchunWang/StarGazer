33fe3565edc76377258c2b694b94830b3b4167a6
StarGazer <wangdchun@qq.com>
2015-12-17 20:18:49 +0800

    Update main.cpp

--- QtExample/MyWord/main.cpp
+++ QtExample/MyWord/main.cpp
@@ -1,1 +1,15 @@
 
+#include "myword.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    //设置显示的字体格式
+    QFont fon("ZYSong18030", 12);
+    a.setFont(fon);
+    MyWord w;
+    w.show();
+
+    return a.exec();
+}
