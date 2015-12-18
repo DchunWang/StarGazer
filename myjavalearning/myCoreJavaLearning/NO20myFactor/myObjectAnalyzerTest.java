123f25eb4c2dbc2d6189e65e7f4757c1eb084bab
StarGazer <DchunWang@users.noreply.github.com>
2015-12-18 12:09:18 +0800

    Update myObjectAnalyzerTest.java

--- myjavalearning/myCoreJavaLearning/NO20myFactor/myObjectAnalyzerTest.java
+++ myjavalearning/myCoreJavaLearning/NO20myFactor/myObjectAnalyzerTest.java
@@ -13,7 +13,7 @@ public class myObjectAnalyzerTest
 {
 	public static void main(String [] args)
 	{
-		ArrayList<Interger> squares = new ArrayList<>();
+		ArrayList<Integer> squares = new ArrayList<>();
 		for(int i = 1; i <= 5; i++)
 			squares.add(i * i);
 		System.out.println(new ObjectAnalyzer().toString(squares));
