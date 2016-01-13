9a38a0a56c32a84784d568e545c8edb522f862a6
StarGazer <DchunWang@users.noreply.github.com>
2016-01-13 20:44:45 +0800

    page 446

---
+++ myjavalearning/myCoreJavaLearning/apple/NotHelloWorld.java
@@ -0,0 +1,19 @@
+package webstart;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class NotHelloWorld extends JApplet
+{
+	public void init()
+	{
+		EventQueue.invokeLater(new Runnable()
+				{
+					public void run()
+					{
+						JLabel label = new JLabel("Not a Hello, World applet", SwingConstants.CENTER);
+						add(label);
+					}
+				});
+	}
+}
