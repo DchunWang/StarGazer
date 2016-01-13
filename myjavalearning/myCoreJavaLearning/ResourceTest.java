a54d786df924d80a266f1d8295d35029660c5a43
StarGazer <DchunWang@users.noreply.github.com>
2016-01-13 20:06:03 +0800

    page 430

---
+++ myjavalearning/myCoreJavaLearning/ResourceTest.java
@@ -0,0 +1,54 @@
+package resource;
+
+import java.awt.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.swing.*;
+
+/**
+ *@page 430
+ *@author wandch
+ *@date 2016-01-13
+ */
+
+public class ResourceTest
+{
+	public static void main(String [] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+				{
+					public void run()
+					{
+						JFrame frame = new ResourceTestFrame();
+						frame.setTitle("ResourceTest");
+						frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+						frame.setVisible(true);
+					}
+				});
+	}
+}
+
+//A frame that loads image and text resources
+
+class ResourceTestFrame extends JFrame
+{
+	private static final int DEFAULT_WIDHT = 300;
+	private static final int DEFAULT_HEIGHT = 300;
+	
+	public ResourceTestFrame()
+	{
+		setSize(DEFAULT_WIDHT, DEFAULT_HEIGHT);
+		URL aboutURL = getClass().getResource("about.gif");
+		Image img = new ImageIcon(aboutURL).getImage();
+		setIconImage(img);
+		
+		JTextArea textArea = new JTextArea();
+		InputStream stream = getClass().getResourceAsStream("about.txt");
+		Scanner in = new Scanner(stream);
+		while(in.hasNext())
+			textArea.append(in.nextLine() + "\n");
+		add(textArea);
+		
+	}
+}
