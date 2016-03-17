b12d6027a35ef44eb8e4b7689a48ec1e421854be
StarGazer <wangdchun@qq.com>
2016-03-17 21:20:30 +0800

    尝试创建第一个MySQL TABLE

---
+++ MySQLExample/first_create_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE employee
+(
+	empployeeNumber	 INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	lastName		 VARCHAR(50) NOT NULL,
+	firstName		 VARCHAR(50) NOT NULL,
+	mobile			 VARCHAR(25) NOT NULL,
+	officeCode	   	 INT(10) NOT NULL,
+	jobTitle		 VARCHAR(50) NOT NULL,
+	birth			 DATETIME NOT NULL,
+	note			 VARCHAR(255) NOT NULL,
+	sex				 VARCHAR(5) NOT NULL,
+	CONSTRAINT office_fk FOREIGN KEY(officeCode) REFERENCES offices(officeCode)
+);
